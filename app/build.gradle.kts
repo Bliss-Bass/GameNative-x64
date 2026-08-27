010f7b63a7a9fc99025d6306064428ea55d79a81
Jon West <electrikjesus@gmail.com>
2026-08-27 19:10:03 -0400

    Bump versionCode to 24 for ax86 device test drops.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         buildConfigField("boolean", "XR_BUILD", "false")
         buildConfigField("boolean", "MODERN_XR", "false")
 
-        versionCode = 22
+        versionCode = 24
         versionName = "1.2.0"
 
         buildConfigField("boolean", "GOLD", "false")
