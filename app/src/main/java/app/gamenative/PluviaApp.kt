dba88b781b42746514272215971dc9cc79f73069
Joshua Tam <297250+joshuatam@users.noreply.github.com>
2026-09-15 03:15:19 +0800

    fix: power control pserver cpu freq detection (#1934)
    * fix: power control pserver cpu freq detection
    
    * Put PowerManager initialize into background thread
    
    * check driver isInitialized on stopPowerControl

--- app/src/main/java/app/gamenative/PluviaApp.kt
+++ app/src/main/java/app/gamenative/PluviaApp.kt
@@ -148,7 +148,9 @@ class PluviaApp : SplitCompatApplication() {
             PlayIntegrity.warmUp(this)
         }
 
-        PowerManager.initialize(this)
+        Thread {
+            PowerManager.initialize(this)
+        }.start()
     }
 
     /**
